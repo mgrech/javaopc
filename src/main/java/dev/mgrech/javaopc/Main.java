eec6c8d246b96d1627e37a3fd27b0f331f05901f
Markus Grech <me@mgrech.dev>
2019-11-03 16:44:09 +0100

    Add support for CLASSPATH environment variable

--- src/main/java/dev/mgrech/javaopc/Main.java
+++ src/main/java/dev/mgrech/javaopc/Main.java
@@ -2,6 +2,10 @@ package dev.mgrech.javaopc;
 
 import com.github.javaparser.Providers;
 import com.github.javaparser.ast.nodeTypes.NodeWithName;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 import javax.tools.ToolProvider;
@@ -12,9 +16,27 @@ import java.util.List;
 
 public class Main
 {
+	private static TypeSolver solverForClassPath() throws IOException
+	{
+		var classPath = System.getenv("CLASSPATH");
+
+		if(classPath == null)
+			return new CombinedTypeSolver(new JavaParserTypeSolver("."), new ReflectionTypeSolver());
+
+		var solver = (TypeSolver)new ReflectionTypeSolver();
+
+		for(var path : classPath.split(";"))
+		{
+			var nextSolver = path.endsWith(".jar") ? new JarTypeSolver(path) : new JavaParserTypeSolver(path);
+			solver = new CombinedTypeSolver(solver, nextSolver);
+		}
+
+		return solver;
+	}
+
 	private static boolean compile(Path sourceFilePath) throws IOException
 	{
-		var cu = JavaOperatorCompiler.parse(Providers.provider(sourceFilePath), new ReflectionTypeSolver());
+		var cu = JavaOperatorCompiler.parse(Providers.provider(sourceFilePath), solverForClassPath());
 
 		if(cu == null)
 			return false;
